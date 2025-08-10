4079e0a6b369cbc045a5e552bdc8c69c720517bb
jennTrev <jenniferroscio3@gmail.com>
2025-08-10 18:40:30 -0400

    first commit 14

--- src/controllers/tecnicoController.js
+++ src/controllers/tecnicoController.js
@@ -62,7 +62,6 @@ export const obtenerTecnico = async (req, res) => {
 
 export const crearTecnico = async (req, res) => {
   try {
-    // req.body debe contener fecha_nacimiento en formato "YYYY-MM-DD"
     const tecnico = await Tecnico.create(req.body)
 
     res.status(201).json({
@@ -101,7 +100,6 @@ export const actualizarTecnico = async (req, res) => {
       })
     }
 
-    // req.body puede incluir fecha_nacimiento para actualizarla
     await tecnico.update(req.body)
 
     res.json({
