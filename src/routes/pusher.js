8de030f10ae37974f2b6274e9726d08d1b2f3f91
jennTrev <jenniferroscio3@gmail.com>
2025-08-30 22:57:43 -0400

    Mensaje descriptivo de los cambios 6

--- src/routes/pusher.js
+++ src/routes/pusher.js
@@ -1,6 +1,6 @@
 // routes/espRoutes.js
 import express from "express";
-import { sendCommand } from "../controllers/pusherController.js";
+import { sendCommand, pusherAuth} from "../controllers/pusherController.js";
 
 const router = express.Router();
 
